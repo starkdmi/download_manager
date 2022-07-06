163269ab5113792614665b53233cf13d4796460d
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2022-07-06 14:24:41 +0300

    Update example.dart

--- example/dart_download_manager/example.dart
+++ example/dart_download_manager/example.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
 import 'package:isolated_download_manager/download_manager.dart';
 
+const directory = "/Users/starkdmi/Downloads/test";
+const links = [
+  "https://golang.org/dl/go1.17.3.src.tar.gz",
+  "https://storage.googleapis.com/dart-archive/channels/stable/release/2.17.5/sdk/dartsdk-macos-arm64-release.zip",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+];
+
 void main() async {
   // Initialize
   final manager = DownloadManager.instance;
   // Here we create `n` amount of long running isolates available for downloader
-  await manager.init(isolates: 1, directory: "/Users/starkdmi/Downloads/test");
+  await manager.init(isolates: 1, directory: directory);
   await Future.delayed(const Duration(seconds: 1));
 
   void dispose() {
@@ -14,9 +21,10 @@ void main() async {
   }
 
   // Download
-  // final url = "https://storage.googleapis.com/dart-archive/channels/stable/release/2.17.5/sdk/dartsdk-macos-arm64-release.zip";
-  final url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-  final request = manager.download(url, path: "/Users/starkdmi/Downloads/test/video.mp4");
+  final request = manager.download(links[2]);
+  // final request = manager.download(links[1], path: "$directory/dart.zip");
+  
+  // Progress
   request.events.listen((event) {
     if (event is DownloadState) {
       print("event: $event");
@@ -40,7 +48,8 @@ void main() async {
   }, onError: (error) {
     print("error $error");
     dispose();
-  }, onDone: () {
+  }, onDone: () async {
+    await Future.delayed(const Duration(milliseconds: 1500));
     dispose();
   });
 
